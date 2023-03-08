624cabc1c84fb04baf7f07bb2999e88e22412761
Prico3 <pricoco.andrea@gmail.com>
2023-03-08 15:44:22 +0100

    First commit + first exercise done

---
+++ src/org/lessons/java/TrainTicket.java
@@ -0,0 +1,30 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class TrainTicket {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Number of km:");
+        int km = Integer.parseInt(scan.nextLine());
+        System.out.println("Age:");
+        int age = Integer.parseInt(scan.nextLine());
+
+
+       double price = km * 0.21;
+        System.out.println("Original price:" + price);
+       double newPrice = 0;
+        if (age < 18){
+            newPrice = (price * 20) / 100;
+        }
+
+        if (age >= 65){
+            newPrice = (price * 40) / 100;
+        }
+        System.out.println("After discount:" + newPrice);
+
+
+    }
+}
