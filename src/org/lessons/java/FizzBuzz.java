81db98a75756354949d9809b6a74cdca44683a27
Prico3 <pricoco.andrea@gmail.com>
2023-03-08 16:36:55 +0100

    end second exercise

---
+++ src/org/lessons/java/FizzBuzz.java
@@ -0,0 +1,21 @@
+package org.lessons.java;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 ){
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+
+
+
+
+        }
+    }
+}
